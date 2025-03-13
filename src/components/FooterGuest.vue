b2ad15f179efd8dbbd7abd76d241e4d28d5e1407
Suhailah Nuriyah Fahma <suhailahnfsella@gmail.com>
2025-03-13 23:36:00 +0700

    add component
    1. NavbarGuest
    2. FooterGuest

---
+++ src/components/FooterGuest.vue
@@ -0,0 +1,68 @@
+<template>
+    <footer class="relative w-full pb-10 pt-10 bg-[#1E1E1E] text-white font-poppins">
+        <div class="container mx-auto">
+            <div class="flex flex-col items-center justify-center w-full py-10 border-t border-[#979797] md:flex-row md:justify-between"></div>
+            <div class="grid justify-between grid-cols-1 gap-4 md:grid-cols-2">
+                <img src="../assets/LenTarioFooter.svg" alt="" width="150" class="ml-10 mt-10">
+                <div class="grid">
+                    <ul class="flex flex-col gap-4">
+                        <li class="flex gap-2 text-xs items-center">
+                            <img src="../assets/iconplace.svg" alt="" width="23">
+                            <p>345 Faulconer Drive, Suite 4 • Charlottesville, CA</p>
+                        </li>
+                        <li class="flex gap-2 text-xs items-center mb-4">
+                            <ul class="flex gap-28">
+                                <li class="flex gap-2 text-xs items-center">
+                                    <img src="../assets/iconphone.svg" alt="" width="23">
+                                    <p>(123) 456-789</p>
+                                </li>
+                                <li class="flex gap-2 text-xs items-center">
+                                    <img src="../assets/iconprint.svg" alt="" width="23">
+                                    <p>(123) 457890</p>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="flex gap-2 text-xs">
+                            <ul class="flex gap-16">
+                                <li class="flex gap-2 text-xs">
+                                    Sosial <br> Media
+                                </li>
+                                <li class="flex gap-6 text-xs">
+                                    <a href=""><img src="../assets/iconfb.svg" alt="" width="23"></a>
+                                    <a href=""><img src="../assets/icontweet.svg" alt="" width="23"></a>
+                                    <a href=""><img src="../assets/iconlinkedin.svg" alt="" width="23"></a>
+                                    <a href=""><img src="../assets/iconyt.svg" alt="" width="23"></a>
+                                    <a href=""><img src="../assets/iconig.svg" alt="" width="23"></a>
+                                    <a href=""><img src="../assets/icongoogleplus.svg" alt="" width="23"></a>
+                                    <a href=""><img src="../assets/iconpinterest.svg" alt="" width="23"></a>
+                                    <a href=""><img src="../assets/iconrss.svg" alt="" width="23"></a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="flex flex-col items-center justify-center w-full py-4 mt-16 border-t border-slate-200 md:flex-row md:justify-between">
+                <div class="flex gap-10 text-xs">
+                    <a href="">ABOUT US</a>
+                    <a href="">CONTACT US</a>
+                    <a href="">HELP</a>
+                    <a href="">PRIVACY POLICY</a>
+                    <a href="">DISCLAIMER</a>
+                </div>
+                <div>
+                    <p class="block mb-4 text-xs text-center text-white md:mb-0">
+                        Copyright © 2025 • Lift Media Inc.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+  
+  <script>
+  export default {
+    name: 'FooterGuest'
+  };
+  </script>
+  
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,17 @@
 <template>
-  <h1>tes</h1>
+  <NavbarGuest activePage="Beranda" />
+  <FooterGuest />
 </template>
   
 <script>
+  import NavbarGuest from '@/components/NavbarGuest.vue'
+  import FooterGuest from '@/components/FooterGuest.vue'
+
   export default {
     name: 'DashBoard',
     components: { 
+      NavbarGuest,
+      FooterGuest
     }
   };
 </script>
