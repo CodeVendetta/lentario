730f2365c4952c7edd9bc213b5afac248b8f1a39
Suhailah Nuriyah Fahma <suhailahnfsella@gmail.com>
2025-03-19 12:25:40 +0700

    update peminjaman ruang barang guest

--- src/views/PeminjamanRuangBarangGuest.vue
+++ src/views/PeminjamanRuangBarangGuest.vue
@@ -1,12 +1,16 @@
 <template>
     <NavbarGuest activePage="Peminjaman ruang & barang" />
     <HeroPeminjamanRuangBarangGuest />
+    <TableQtyPinjamRuangGuest />
+    <TableQtyPinjamBarangGuest />
     <FooterGuest />
   </template>
     
   <script>
     import NavbarGuest from '@/components/NavbarGuest.vue'
     import HeroPeminjamanRuangBarangGuest from '@/components/HeroPeminjamanRuangBarangGuest.vue'
+    import TableQtyPinjamRuangGuest from '@/components/TableQtyPinjamRuangGuest.vue'
+    import TableQtyPinjamBarangGuest from '@/components/TableQtyPinjamBarangGuest.vue'
     import FooterGuest from '@/components/FooterGuest.vue'
   
     export default {
@@ -14,6 +18,8 @@
       components: { 
         NavbarGuest,
         HeroPeminjamanRuangBarangGuest,
+        TableQtyPinjamRuangGuest,
+        TableQtyPinjamBarangGuest,
         FooterGuest
       }
     };
