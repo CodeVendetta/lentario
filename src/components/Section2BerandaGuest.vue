1496e69979277e3606dde91fae517ded186cd9ff
Suhailah Nuriyah Fahma <suhailahnfsella@gmail.com>
2025-03-19 12:04:40 +0700

    update beranda guest

--- src/components/Section2BerandaGuest.vue
+++ src/components/Section2BerandaGuest.vue
@@ -1,18 +1,13 @@
 <template>
     <div class="w-full mt-[-20px2] text-[#1F1F1F] font-poppins min-h-28 flex items-center justify-center bg-[#1E1E1E] gap-8 px-10 py-10">
         <div class="flex bg-white px-6 py-5 items-center gap-5 rounded-md">
-            <img src="../assets/iconsample.svg" alt="" width="60">
+            <img class="bg-[#0C8CE9] p-3 rounded-full" src="../assets/iconruangberanda.svg" alt="" width="60">
             <p class="text-[#1E1E1E] font-semibold">Peminjaman Ruang</p>
             <a href=""><img src="../assets/iconnext.svg" alt="" width="30"></a>
         </div>
         <div class="flex bg-white px-6 py-5 items-center gap-5 rounded-md">
-            <img src="../assets/iconsample.svg" alt="" width="60">
+            <img class="bg-[#0C8CE9] p-3 rounded-full" src="../assets/iconbarangberanda.svg" alt="" width="60">
             <p class="text-[#1E1E1E] font-semibold">Peminjaman Barang</p>
-            <a href=""><img src="../assets/iconnext.svg" alt="" width="30"></a>
-        </div>
-        <div class="flex bg-white px-6 py-5 items-center gap-5 rounded-md">
-            <img src="../assets/iconsample.svg" alt="" width="60">
-            <p class="text-[#1E1E1E] font-semibold">Denah Ruangan</p>
             <a href=""><img src="../assets/iconnext.svg" alt="" width="30"></a>
         </div>
     </div>
